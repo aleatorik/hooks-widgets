656532958226be4064b4e244ed436f5081af8497
daseul song <dssong@laivdata.com>
2021-05-05 21:11:33 +0900

    Add some comments

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,21 +1,33 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState("programming");
+  const [term, setTerm] = useState('programming');
   const [results, setResults] = useState([]);
 
   useEffect(() => {
+    // <1> First method - declare helper function
     const search = async () => {
-      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
         params: {
-          action: "query",
-          list: "search",
-          origin: "*",
-          format: "json",
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
           srsearch: term,
         },
       });
+
+      // <2> Second method -> there's no performance benefits compared to the first one, just consider which more easier to use is
+      // (async () => {
+      //   await axios.get("https://en.wikipedia.org/w/api.php");
+      // })();
+
+      // <3> Third method
+      // axios.get("https://en.wikipedia.org/w/api.php")
+      //   .then((response) =>
+      //     console.log(response.data);
+      // });
 
       setResults(data.query.search);
     };
@@ -25,7 +37,8 @@ const Search = () => {
     } else {
       const timeoutId = setTimeout(() => {
         if (term) {
-          // useEffect의 초기렌더링으로 인해 빈 공백을 위키피디아 넣을 때 나오는 에러를 갖게되므로, 초기값이 있을 때, 즉 term에 값이 주어졌을 때만 위의 helper function이 실행되도록 조건문을 거는 것. -> 에러해결
+          // useEffect의 초기렌더링으로 인해 빈 공백을 위키피디아 넣을 때 나오는 에러를 갖게되므로,
+          // 초기값이 있을 때, 즉 term에 값이 주어졌을 때만 위의 helper function이 실행되도록 조건문을 거는 것. -> 에러해결
           search();
         }
       }, 1000);
@@ -49,7 +62,11 @@ const Search = () => {
         <div className="content">
           <div className="header">{result.title}</div>
           <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
-          {/* 'dangerouslySetInnerHTML'를 통해 XXS Attack이라 불리는 'Cross site scripting attack'으로 인해 안전상의 구멍을 열어놓을 수 있는 리스크가 생김. 신뢰할 수 있는 URL에 경우에만 위에 dangerouslySetInnerHtml 사용할 것. 왜냐하면 third party가 보내는 html이 개발자 도구에 노출되기 때문에. 이 경우, 위키피디아를 신뢰할 수 있는가? 그들이 보내는 HTML 내용을 신뢰하는가? 해커에 의해 어떤 계정을 해킹하기 위해 앱 안에 유해한 기능의 JS 코드가 심어질 수 있음  */}
+          {/* 'dangerouslySetInnerHTML'를 통해 XXS Attack이라 불리는 'Cross site scripting attack'으로 인해 
+          안전상의 구멍을 열어놓을 수 있는 리스크가 생김. 신뢰할 수 있는 URL에 경우에만 위에 dangerouslySetInnerHtml 사용할 것. 
+          왜냐하면 third party가 보내는 html이 개발자 도구에 노출되기 때문에. 
+          이 경우, 위키피디아를 신뢰할 수 있는가? 그들이 보내는 HTML 내용을 신뢰하는가? 
+          해커에 의해 어떤 계정을 해킹하기 위해 앱 안에 유해한 기능의 JS 코드가 심어질 수 있음  */}
         </div>
       </div>
     );
